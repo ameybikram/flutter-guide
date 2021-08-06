d6e6c41001080d29a4ff1d6bbffb50cb1de979f2
Amey <a.bikram492@gmail.com>
2021-08-06 23:16:08 +0530

    This commit is of section 2 part 29

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      /* Keep in mind that Flutter controls the entire UI, all the pixels on the screen are controlled by your Flutter app in the end, by the Flutter framework. It does not take an existing mobile app and just mix in some widgets or some components, it controls the entire app instead and therefore you also have to give clear instructions about every detail,what should be the background color, should there be an appBar? And to do that, there is a little helper widget which you can use and I'll replace my text widget here with it and that's scaffold. Now scaffold is yet another widget which is baked into material.dart. So it will give you a basic design and structure and color scheme or coloring for giving you a UI that looks more like a regular mobile app page. Scaffold also has a couple of named arguments*/
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Text(
+            'This is my default text'), // body widget which is the body of the screen takes only one widget at a time and what if we want to add more widgets like buttons and so on.
+        /* For this, it's important to understand that we have different types of widgets in Flutter. We have the visible widgets which are related to user input and to outputting data, things like a button or a text or a card and we have only seen the text from all these examples here thus far but there are more widgets like the RaisedButton which renders a button and other widgets and we'll see all of those throughout the course. Now these are the widgets which we see, we see a button, we see the text right, that is what we see and that is of course crucial for any application but equally crucial are invisible widgets that help us with layout and with controlling how our widget tree behaves and how it looks like and there we got things like row, column, ListView and so on. These are also widgets that ship with Flutter which we don't have to build ourselves, which we don't see themselves but which help us with structuring our content. So these widgets give our app structure and control how visible widgets are drawn onto the screen and therefore of course, they're super important. There also is a very important widget which also ships with Flutter, the container widget which kind of belongs into both categories as you will learn once we use it because it by default is invisible but you can also give it some styling so that you can see it */
       ),
     );
   }
