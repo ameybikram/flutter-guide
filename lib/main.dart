27e6e614832abcc862df2da1d2ca8f1470d2a2ad
Amey <a.bikram492@gmail.com>
2021-08-16 03:26:13 +0530

    section 2 part 51

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'package:flutter_complete_guide/result.dart';
+import 'quiz.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,54 +21,46 @@ class MyApp extends StatefulWidget {
 // Difference between MyAppState and _MyAppState is that the latter cannot be accessed from another file and it's property cannot be accessed or mutated from other files and this class becomes private whereas classes name with no underscore is public and is accessible from any file.
 // Class Name/ Properties/ Methods name with underscore
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favourite cricketer?',
+      'answers': ['Sachin', 'Virat', 'Rohit', 'Dhoni'],
+    },
+  ];
   var _questionIndex = 0; // Called properties
   void _answerQuestion() {
     setState(() {
       // setState is use to call build method again for the widget where it is called
       // To re render the state when onPressed is called hence to change the state
-      // if (_questionIndex < 1) {
-      _questionIndex++;
-      // }
+      if (_questionIndex < _questions.length) {
+        _questionIndex++;
+      }
     });
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favourite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'Who\'s your favourite cricketer?',
-        'answers': ['Sachin', 'Virat', 'Rohit', 'Dhoni'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              // questions.elementAt(0),
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questions: _questions,
+                questionIndex: _questionIndex,
+              )
+            : Result(),
       ),
     );
   }
