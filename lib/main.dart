271884d54e7d08c1a35d8813e8f353fc0f3962b5
Amey <a.bikram492@gmail.com>
2021-08-18 19:38:55 +0530

    part 72

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/result.dart';
-import 'quiz.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // createState returns State Object
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-// State widget cannot be re-built
-// First of all, state is a generic type so we should add angled brackets there and in between, we add a pointer
-// at our class here,
-// Difference between MyAppState and _MyAppState is that the latter cannot be accessed from another file and it's property cannot be accessed or mutated from other files and this class becomes private whereas classes name with no underscore is public and is accessible from any file.
-// Class Name/ Properties/ Methods name with underscore
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favourite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1}
-      ],
-    },
-    {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 10},
-        {'text': 'Snake', 'score': 1},
-        {'text': 'Elephant', 'score': 9},
-        {'text': 'Lion', 'score': 5}
-      ],
-    },
-    {
-      'questionText': 'Who\'s your favourite cricketer?',
-      'answers': [
-        {'text': 'Sachin', 'score': 10},
-        {'text': 'Virat', 'score': 9},
-        {'text': 'Rohit', 'score': 9},
-        {'text': 'Dhoni', 'score': 10}
-      ],
-    },
-  ];
-  var _questionIndex = 0; // Called properties
-  var _totalScore = 0;
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      // setState is use to call build method again for the widget where it is called
-      // To re render the state when onPressed is called hence to change the state
-      if (_questionIndex < _questions.length) {
-        _questionIndex++;
-      }
-    });
-    print(_questionIndex);
-  }
-
-  void resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questions: _questions,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore, resetQuiz),
-      ),
+      title: 'Flutter App',
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: [
+            // Card by defaut assumes the size of its child unless unless you have a parent or a card like container, which has its own clearly defined width. This also means that column doesn't seem to be such a parent and indeed column is a widget which only takes as much width as its children need. So if we want to change the size of that card here, then we need to change the size of child. Now text on the other hand is a widget which by default only takes as much space as this text needs and if you want to change the size of text, you need to change the size of its parent. So what can we do here since this card depends on the child and this text depends on the parent? We have a dependency which is kind of hard to break, right, There are two ways of breaking that one is that we wrap that text with a widget where we can set a size and we can do it with the refactoring shortcut which you can find in the key bindings in case you forgot it and there we can wrap the text with a container because container is the most universal styling positioning sizing widget you have. With a container, you can control almost anything when it comes to how something is sized or how something is aligned and styled, at least if we talk about things like background colors and so on and therefore, once I wrap the text with a container, we can use the fact that on the container we can set a width and we could set this to 100 and this means the container will now have the width of 100 device pixels which is a unit that always gives us the same size even on differently sized phones or devices.
+            Card(
+              child: Container(
+                  color: Colors.blue, width: 100, child: Text('Chart!')),
+            ),
+            Card(
+              child: Text('List of TX'),
+            )
+          ],
+        ));
   }
 }
