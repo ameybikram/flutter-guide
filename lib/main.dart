04e8132c068b73689e6d6e0709a3d6f4282100ad
Amey <a.bikram492@gmail.com>
2021-08-19 14:52:34 +0530

    part 86 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'transaction.dart';
+import 'models/transaction.dart';
 import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
@@ -31,6 +31,12 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  // String titleInput;
+  // String amountInput;
+  // TextEditingController() is a class provided by flutter
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +45,7 @@ class MyHomePage extends StatelessWidget {
         ),
         // Columns are there to take two or more widget and position them above each other now matter how much vertical space is there and items/widgets next to each other means Row. styling in Flutter works entirely through the arguments you pass to your widgets. and if a certain widget doesn't take a certain argument, if you can't give a column a background color for example, indeed you can't, then you always have to use another widget and wrap the widget which you want to style with it.
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment:
               CrossAxisAlignment.stretch, // It starts from the end
           children: [
@@ -49,6 +55,42 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
                 child: Text('Chart!'),
                 elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                // If we need some padding, there is a dedicated Padding() widget - basically a simplified Container()
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                      ),
+                      // Flutter automatically connects the controller with our text fields and these controllers in the end listen to the user input and save the user input
+                      controller: titleController,
+                      // onChanged: (val) {
+                      //   titleInput = val;
+                      // },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Amount',
+                      ),
+                      controller: amountController,
+                      // onChanged: (val) {
+                      //   amountInput = val;
+                      // }, // onChaned is fired with every keystroke
+                    ),
+                    FlatButton(
+                      child: Text('Add Trasaction'),
+                      onPressed: () => {print(titleController.text)},
+                      textColor: Colors.purple,
+                    ),
+                  ],
+                ),
               ),
             ),
             Column(
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/transaction.dart';
+
+class TransactionList extends StatefulWidget {
+  // const TransactionList({ Key? key }) : super(key: key);
+
+  @override
+  _TransactionListState createState() => _TransactionListState();
+}
+
+class _TransactionListState extends State<TransactionList> {
+  final List<Transaction> _userTransaction = []
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
